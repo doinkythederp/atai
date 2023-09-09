d6bda735578c8ac8d83b035b920180cf5867cc0b
doinkythederp <doinkythederp@icloud.com>
2023-09-08 18:41:37 -0700

    feat: make nix build work on linux

--- derivation.nix
+++ derivation.nix
@@ -1,13 +1,14 @@
 { rustPlatform, fetchgit, lib, pkgs, ... }:
 
 rustPlatform.buildRustPackage {
-    pname = "atai";
-    version = "0.1.0";
+  pname = "atai";
+  version = "0.1.0";
 
-    src = ./.;
+  src = ./.;
 
-    cargoHash = "";
+  cargoHash = "sha256-baogO0MT1fDCd9f1y3H6QqVJQQxPoQcOx/9NnfkjzsE=";
 
-    buildInputs = with pkgs; [
-    ];
+  buildInputs = with pkgs; [ ];
+
+  doCheck = false;
 }
